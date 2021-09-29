a1b177a760013a21b1d5782813154b8de8c2ab55
rahul3899 <80642505+rahul3899@users.noreply.github.com>
2021-09-29 16:52:20 +0530

    Update Patient.sql

--- src/main/resources/Sql Query/Patient.sql
+++ src/main/resources/Sql Query/Patient.sql
@@ -1,40 +1,16 @@
---------------------------------------------------------
---  File created - Wednesday-September-29-2021   
---------------------------------------------------------
---------------------------------------------------------
---  DDL for Table PATIENT
---------------------------------------------------------
+CREATE TABLE patient
+   (	"id" NUMBER(10), 
+	"address" VARCHAR2(255), 
+	"age" NUMBER(10),
+	"emailId" VARCHAR2(255), 
+	"gender" VARCHAR2(255), 
+	"medicalHistory" VARCHAR2(255), 
+	"name" VARCHAR2(255), 
+	"phoneNumber" VARCHAR2(255),
+    PRIMARY KEY(id)
+   );
 
-  CREATE TABLE "ROOT"."PATIENT" 
-   (	"ID" NUMBER(10,0), 
-	"ADDRESS" VARCHAR2(255 CHAR), 
-	"AGE" NUMBER(10,0), 
-	"EMAIL_ID" VARCHAR2(255 CHAR), 
-	"GENDER" VARCHAR2(255 CHAR), 
-	"MEDICAL_HISTORY" VARCHAR2(255 CHAR), 
-	"NAME" VARCHAR2(255 CHAR), 
-	"PHONE_NUMBER" VARCHAR2(255 CHAR)
-   ) SEGMENT CREATION IMMEDIATE 
-  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
-  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
-  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
-  TABLESPACE "USERS" ;
---------------------------------------------------------
---  DDL for Index SYS_C007306
---------------------------------------------------------
 
-  CREATE UNIQUE INDEX "ROOT"."SYS_C007306" ON "ROOT"."PATIENT" ("ID") 
-  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
-  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
-  TABLESPACE "USERS" ;
---------------------------------------------------------
---  Constraints for Table PATIENT
---------------------------------------------------------
+INSERT INTO patient (id, address, age, emailId,gender, medicalHistory, name, phoneNumber) VALUES ('1', 'jabalpur', '25', 'r@gmail.com','male','Diabetes','Rahul','555555555');
 
-  ALTER TABLE "ROOT"."PATIENT" ADD PRIMARY KEY ("ID")
-  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
-  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
-  TABLESPACE "USERS"  ENABLE;
-  ALTER TABLE "ROOT"."PATIENT" MODIFY ("ID" NOT NULL ENABLE);
+INSERT INTO patient (id, address, age, emailId,gender, medicalHistory, name, phoneNumber) VALUES ('2', 'Indore', '28', 'pag@gmail.com','female','lung problem','raj','4445555');
